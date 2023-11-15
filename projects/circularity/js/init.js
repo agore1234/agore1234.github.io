f444e70d014bf48ac604e7835000518918c50f2f
Aidenn Gore <apg0389969@lpssonline.com>
2023-11-15 13:52:55 +0000

    update

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -24,10 +24,12 @@ var init = function (window) {
         var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle {
         circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
         physikz.addRandomVelocity(circle, canvas);
         view.addChild(circle);
         circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
         for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
